81c19244f9740a8b2b2d6a66615c6385c41d3861
Igor Costa <igor.vc30@gmail.com>
2020-05-02 00:31:02 -0300

    feature: list all transaction and show the balance

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+export const formatValue = (value: number): string =>
+  Intl.NumberFormat('pt-br', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  );
 
-export default formatValue;
+export const formatDate = (date: Date): string =>
+  Intl.DateTimeFormat('pt-br').format(new Date(date));
